0277ac16bbe3c97568508a6018696c08afcf1b1e
Igor <igrr69@gmail.com>
2018-06-12 18:44:09 +0300

    Add alignment to center for the table

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Table files={["hello", "world"]} />
+        <div className="flex justify-center">
+          <Table files={["hello", "world"]} />
+        </div>
       </div>
     );
   }
